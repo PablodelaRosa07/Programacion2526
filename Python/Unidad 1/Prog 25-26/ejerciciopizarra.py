7a491af717ff93919da5248958c88330a8f5901e
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-09-30 08:23:51 +0200

    Subo el archivoç

---
+++ Python/Unidad 1/Prog 25-26/ejerciciopizarra.py
@@ -0,0 +1,11 @@
+opcion = (input("Dame una opción (A, B, C o D):"))
+if opcion == "A":
+    print("Alta")
+elif opcion == "B":
+    print("Baja")
+elif opcion == "C":
+    print("Cambio")
+elif opcion == "D":
+    print("No válido")
+else :
+    print("No es una de las 4 opciones")
