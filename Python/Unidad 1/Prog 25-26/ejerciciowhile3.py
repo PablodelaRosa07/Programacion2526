ab02a85f92527968ed3231823c05d921dd6ab3b5
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-10-07 17:23:42 +0200

    subo

---
+++ Python/Unidad 1/Prog 25-26/ejerciciowhile3.py
@@ -0,0 +1,5 @@
+num = 11
+while num > 0 and num <12:
+    num = num-1
+    print(num)
+print("Fin")
---
+++ Python/Unidad 1/Prog 25-26/ejerciciowhile4.py
@@ -0,0 +1,6 @@
+num = float(input("Introduce un número:"))
+while num != 0:
+    num2 = float(input("Introduce otro número:"))
+    num = num+num2
+    if num2 ==0:
+        print("El resultado es:",num)
