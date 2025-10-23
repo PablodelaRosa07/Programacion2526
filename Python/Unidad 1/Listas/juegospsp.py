3505de038095809cb9fb6da70c4ad705c4ca574a
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-10-23 17:25:52 +0200

    subo

--- Python/Unidad 1/Listas/juegospsp.py
+++ Python/Unidad 1/Listas/juegospsp.py
@@ -1,5 +1,25 @@
 print("R. Registrar juegos")
 print("E. Mostrar estadísticas")
-print("Salir del programa")
-opcion = input("¿Qué desea hacer?:").lower
-if opcion == "":
+print("S. Salir del programa")
+nombre = []
+puntuacion=[]
+genero=[]
+numjuegos = 0
+opcion = input("¿Qué desea hacer?:")
+while opcion == "r" or opcion == "e":
+    if opcion == "r":
+        print("Registrando...")
+        nom= (input("Introduce el nombre del juego:"))
+        punt= int(input("Introduce tu puntuación (1-10):"))
+        gen= input("Introduce el género del juego:")
+        nombre.append[nom]
+        puntuacion.append[punt]
+        genero.append[gen]
+        numjuegos = numjuegos+1
+    if opcion == "e":
+        for i in range (numjuegos):
+            print("Mostrando tu colleción de juegos:")
+            print(nombre[i],"| Puntuación:",puntuacion[i],"| Género:",genero[i])
+while opcion != "r" and opcion != "e" and opcion != "s":
+    opcion = input("¿Qué desea hacer?:")
+print("Saliendo del programa")
