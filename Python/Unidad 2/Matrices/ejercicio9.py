3918afb74a1c95cb6fccd57cc02600b5ff5c93c4
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-11-25 17:38:02 +0100

    subo

---
+++ Python/Unidad 2/Matrices/ejercicio9.py
@@ -0,0 +1,28 @@
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+def devuelveDiagonalPrincipal(matriz):
+    lista = []
+    b = 0
+    for i in range (0,len(matriz)):
+        for a in range (0,len(matriz)):
+            if a == b:
+                lista.append(matriz[i][a])
+        b = b+1
+
+    return lista
+
+def devuelveDiagonalSecundaria(matriz):
+    listaInversa = []
+    b = 2
+    for i in range (0,len(matriz)):
+        for a in range (0,len(matriz)):
+            if a == b:
+                listaInversa.append(matriz[i][a])
+        b = b-1
+
+    return listaInversa
+
+diagonalPrincipal = devuelveDiagonalPrincipal(matriz)
+diagonalSecundaria = devuelveDiagonalSecundaria(matriz)
+print(diagonalPrincipal)
+print(diagonalSecundaria)
